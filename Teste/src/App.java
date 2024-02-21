5ef54d51213c660b5e352de1c5e6d49a0af26941
vascogaspar <vblcg@iscte-iul.pt>
2024-02-21 16:13:06 +0000

    branch test

--- Teste/src/App.java
+++ Teste/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("branch test");
         System.out.println("Teste do branch");
     }
 }
